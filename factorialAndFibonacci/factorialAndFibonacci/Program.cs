ba1129686a7dcd81d67dea2e120b13a4b53d60e4
CoDeReD72 <47104283+CoDeReD72@users.noreply.github.com>
2023-07-29 21:37:56 +0100

    Init

---
+++ factorialAndFibonacci/factorialAndFibonacci/Program.cs
@@ -0,0 +1,111 @@
+﻿
+
+
+
+
+// == TESTING ==
+static void TestFactorial()
+{
+    Console.WriteLine("=== FACTORIAL TESTS ===");
+
+    Console.WriteLine("Test 1 - Factorial of 0");
+    Console.WriteLine("\t - Test Criteria: Factorial(0) should return 1.");
+    if (factorial(0) == 1)
+    {
+        Console.WriteLine("\t - Pass");
+    }
+    else
+    {
+        Console.WriteLine("\t - Fail");
+    }
+
+    Console.WriteLine("Test 2 - Factorial of 5");
+    Console.WriteLine("\t - Test Criteria: Factorial(5) should return 120.");
+    if (factorial(5) == 120)
+    {
+        Console.WriteLine("\t - Pass");
+    }
+    else
+    {
+        Console.WriteLine("\t - Fail");
+    }
+
+    Console.WriteLine('\n');
+    // Add more test cases as needed
+}
+
+static void TestFibonacci()
+{
+    Console.WriteLine("=== FIBONACCI TESTS ===");
+    Console.WriteLine("Test 1 - Fibonacci at position 0");
+    Console.WriteLine("\t - Test Criteria: Fibonacci(0) should return 0.");
+    if (fibonacci(0) == 0)
+    {
+        Console.WriteLine("\t - Pass");
+    }
+    else
+    {
+        Console.WriteLine("\t - Fail");
+    }
+
+    Console.WriteLine("Test 2 - Fibonacci at position 5");
+    Console.WriteLine("\t - Test Criteria: Fibonacci(5) should return 5.");
+    if (fibonacci(5) == 5)
+    {
+        Console.WriteLine("\t - Pass");
+    }
+    else
+    {
+        Console.WriteLine("\t - Fail");
+    }
+
+    Console.WriteLine('\n');
+    // Add more test cases as needed
+}
+
+static int fibonacci(int n)
+{
+    if (n < 0)
+    {
+        throw new ArgumentException("Fibonacci sequence is not defined for negative numbers.");
+    }
+
+    // Base cases: Fibonacci(0) = 0 and Fibonacci(1) = 1
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+
+    // Recursive case
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+
+static int factorial(int n)
+{
+
+    // Error handling - Negative number
+    if (n < 0)
+    {
+        throw new Exception("Negative Number Inputted");
+    }
+
+    // Base case: factorial of 0 is 1
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    // Recursive case: n! = n * (n-1)!
+    return n * factorial(n - 1);
+
+}
+
+
+TestFactorial();
+
+TestFibonacci();
---
+++ reverseString/reverseString/Program.cs
@@ -0,0 +1,63 @@
+﻿static string reverseString(string s)
+{
+    string finalString = null;
+
+    if (s != "")
+    {
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            finalString += s[i];
+        }
+
+
+        return finalString;
+    }
+
+    else
+    {
+        return "";
+    }
+
+}
+
+static void tests()
+{
+    // Test 1
+    string input1 = "hello world";
+    string expected1 = "dlrow olleh";
+    Console.WriteLine("Test 1: Hello World");
+    Console.WriteLine("\t - Should Equal: " + expected1);
+    if (reverseString(input1) == expected1) { Console.WriteLine("[/] - Test Passed"); } else { Console.WriteLine("[X] - Test Failed"); }
+
+    // Test 2 - Empty string
+    string input2 = "";
+    string expected2 = "";
+    Console.WriteLine("Test 2: Empty string");
+    Console.WriteLine("\t - Should Equal: " + expected2);
+    if (reverseString(input2) == expected2) { Console.WriteLine("[/] - Test Passed"); } else { Console.WriteLine("[X] - Test Failed"); }
+
+    // Test 3 - Single character
+    string input3 = "a";
+    string expected3 = "a";
+    Console.WriteLine("Test 3: Single character");
+    Console.WriteLine("\t - Should Equal: " + expected3);
+    if (reverseString(input3) == expected3) { Console.WriteLine("[/] - Test Passed"); } else { Console.WriteLine("[X] - Test Failed"); }
+
+    // Test 4 - Mixed case input
+    string input4 = "ReVeRsE";
+    string expected4 = "EsReVeR";
+    Console.WriteLine("Test 4: Mixed case input");
+    Console.WriteLine("\t - Should Equal: " + expected4);
+    if (reverseString(input4) == expected4) { Console.WriteLine("[/] - Test Passed"); } else { Console.WriteLine("[X] - Test Failed"); }
+
+    // Test 5 - String with spaces
+    string input5 = "    hello    world    ";
+    string expected5 = "    dlrow    olleh    ";
+    Console.WriteLine("Test 5: String with spaces");
+    Console.WriteLine("\t - Should Equal: " + expected5);
+    if (reverseString(input5) == expected5) { Console.WriteLine("[/] - Test Passed"); } else { Console.WriteLine("[X] - Test Failed"); }
+
+}
+
+tests();
+
