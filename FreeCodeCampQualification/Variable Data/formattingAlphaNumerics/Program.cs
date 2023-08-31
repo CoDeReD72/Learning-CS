71b393a62946e72dcc72131f75aeb84a62ab314b
CoDeReD72 <47104283+CoDeReD72@users.noreply.github.com>
2023-08-31 03:19:35 +0100

    2023-08-31-03-19

---
+++ FreeCodeCampQualification/Variable Data/formattingAlphaNumerics/Program.cs
@@ -0,0 +1,20 @@
+﻿// String Interpolation
+
+string first = "Hello";
+string second = "World";
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
+
+// Formatting
+// {amount:C} - Currency
+// {amount:N} - Number
+// {amount:P} - Percentage
+
+// Padding
+
+string input = "Pad this";
+Console.WriteLine(input.PadLeft(12));
+
+//PadLeftOrRight(totalLength, 'character')
+// PadLeft - Makes the total length of the string x by padding to the left
+// PadRight - Same but for the right
